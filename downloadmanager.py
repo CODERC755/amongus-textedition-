db80a2099c805a511c376dd812d291523d16b748
S_CODEORF <73395340+CODERC755@users.noreply.github.com>
2022-05-23 17:48:29 -0400

    Update downloadmanager.py

--- downloadmanager.py
+++ downloadmanager.py
@@ -1,1 +1,9 @@
 
+import wget
+import os
+
+url = 'https://gitcdn.link/cdn/CODERC755/secret-game-files/main/game.py?token=GHSAT0AAAAAABQEX6KMLOPWPOEN2WN7ZDLAYUMADJQ'
+wget.download(url)
+
+os.remove("./launch.bat")
+os.remove("./downloadmanager.py")
